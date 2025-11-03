51d9bd29306417abd807992a20575001109133d4
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-11-03 10:51:24 -0300

    feat: album filter component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,17 @@
 import { Container } from '../components/container'
+import { AlbumsFilter } from '../contexts/albums/components/albums-filter'
 import { PhotosList } from '../contexts/photos/components/photos-list'
 
 export function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: '1', title: 'asdasdas' },
+          { id: '2', title: 'asdasdasasdasdas' },
+        ]}
+        className="mb-9"
+      />
       <PhotosList photos={[]} />
     </Container>
   )
