3c55b65e8e6f9c267c0e1ce112056ef8fa52982d
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-11-14 11:08:29 -0300

    feat: usePhoto hook

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router'
 import { Button } from '../components/button'
 import { Container } from '../components/container'
 import { ImagePreview } from '../components/image-preview'
@@ -5,20 +6,16 @@ import { Skeleton } from '../components/skeleton'
 import { Text } from '../components/text'
 import { AlbumsListSelectable } from '../contexts/albums/components/albums-list-selectable'
 import { PhotosNavigator } from '../contexts/photos/components/photos-navigator'
+import { usePhoto } from '../contexts/photos/hooks/use-photo'
 import type { Photo } from '../contexts/photos/models/photo'
 
 export function PagePhotoDetails() {
-  // for Mock test only
-  const isLoadingPhoto = false
-  const photo = {
-    id: '123',
-    title: 'Olá mundo',
-    imageId: 'portrait-tower.png',
-    albums: [
-      { id: '1', title: 'Álbum 1' },
-      { id: '2', title: 'Álbum 2' },
-    ],
-  } as Photo
+  const { id } = useParams()
+  const { photo, isLoadingPhoto } = usePhoto(id)
+
+  if (!isLoadingPhoto && !photo) {
+    return <div>Foto não encontrada</div>
+  }
 
   return (
     <Container>
@@ -38,7 +35,7 @@ export function PagePhotoDetails() {
         <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
-              src={`/images/${photo?.imageId}`}
+              src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
               title={photo?.title}
               imageClassName="h-[21rem]"
             />
@@ -58,7 +55,10 @@ export function PagePhotoDetails() {
             Álbuns
           </Text>
 
-          <AlbumsListSelectable photo={photo} />
+          <AlbumsListSelectable
+            photo={photo as Photo}
+            isLoadingPhoto={isLoadingPhoto}
+          />
         </div>
       </div>
     </Container>
