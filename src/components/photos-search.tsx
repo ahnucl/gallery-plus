732d8f915f7836d0ac37a167c9683db438a185fd
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-11-14 10:40:09 -0300

    feat: text filter

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -1,15 +1,20 @@
 import { useCallback, useState } from 'react'
 import SearchIcon from '../assets/icons/search.svg?react'
+import { usePhotos } from '../contexts/photos/hooks/use-photos'
 import { debounce } from '../helpers/utils'
 import { InputText } from './input-text'
 
 export function PhotosSearch() {
   // Aqui de fato React seria um módulo, seria preciso importar ele no lugar do useState
   const [inputValue, setInputValue] = useState('')
+  const { filters } = usePhotos()
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSetValue = useCallback(
-    debounce((value: string) => console.log('Valor com debounce', value), 500),
-    []
+    debounce((value: string) => {
+      filters.setQ(value)
+    }, 500),
+    [filters.setQ]
   )
 
   // Aqui React é um namespace, só está importanto a tipagem
